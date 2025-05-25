92d4194515a5b0ee4ed2f1cc0ff6c3bcfbbb6b79
AleksStoimenov <aleks.stoimenov@abv.bg>
2025-05-25 23:10:54 +0300

    FinalResult2

---
+++ toggleMode.js
@@ -0,0 +1,13 @@
+function toggleTheme() {
+    const themeLink = document.getElementById('theme-style');
+    const currentTheme = themeLink.getAttribute('href');
+    currentTheme="styles.css";
+    if (currentTheme === 'styles.css') {
+        themeLink.setAttribute('href', 'light_style.css');
+        localStorage.setItem('theme', 'light');
+    } else {
+        themeLink.setAttribute('href', 'styles.css');
+        localStorage.setItem('theme', 'dark');
+    }
+
+};
